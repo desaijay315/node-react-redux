4fc7c4e0373b4d9fb720c3af4bbcf00581d9abfa
jay <desaijay315@gmail.com>
2018-05-05 21:03:24 +0530

    repo change

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+//dev.js - dont commit 	this!!
+
+module.exports = {
+	GOOGLE_CLIENT_ID  : '175678180677-bhgmheinbvaotnk5p0dhu8rsh208kphf.apps.googleusercontent.com',
+	GOOGLE_CLIENT_SECRET : '-98whkXCcIzL1rL8s-432Z-V',
+	MONGO_URI :'mongodb://desaijay:desaijay@ds215910.mlab.com:15910/emaily-dev',
+	COOKIE_KEY : 'flsdsdfsdfsdf3443lk3454k6l5k765lkl45756756eeetertert'
+};
--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const express = require('express');
-const app  = express();
+const express        = require('express');
+const mongoose       = require('mongoose');
+const cookieSession  = require('cookie-session');
+const passport  	 = require('passport');
+const keys           = require('./config/keys');
+const app            = express();
 
-app.get('/', (req,res) => {
-	res.send({hi : 'u there'});
-});
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.MONGO_URI);
+
+app.use(
+	cookieSession({
+		maxAge : 30 * 24 * 60 * 60 * 1000,
+		keys : [keys.COOKIE_KEY]
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
