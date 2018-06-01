730b5f8bbea62f1480e8cf1830f23d829c2a0eb8
jay <desaijay315@gmail.com>
2018-06-01 09:07:25 +0530

    git add

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'; 
+
+const Header    = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing   = () => <h2>Landing</h2>
 
 const App = () => {
-    return (
+    return ( 
         <div>
-            <h1>Hi There!</h1>
+            <BrowserRouter>
+                <div>
+                    <Route exact path="/" component = {Landing} />
+                    <Route path="/surveys" component = {Dashboard} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
